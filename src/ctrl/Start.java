e3c7abee182f6c62012de4bdac5e2996f91d4d74
mcmaceac <mcm.maceachern@gmail.com>
2018-01-15 23:37:54 -0500

    Initial form looking the way the prof has it

--- src/ctrl/Start.java
+++ src/ctrl/Start.java
@@ -16,11 +16,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet({"/Start", "/Startup", "/Startup/*"})
 public class Start extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	//below variables are used so the client can recalculate payments with differing interest rates
-	private double savedPrincipal, savedPeriod;
-	
-	private boolean varsInitialized = false;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -36,10 +31,10 @@ public class Start extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		//System.out.println("doGet called!");
-		//OSAP parameters
 		response.sendRedirect(this.getServletContext().getContextPath() + "/UI.jspx");
 		double principal, period, interest, fixedInterest, graceInterest, gracePeriod;
+		
+		//boolean gracePeriodEnabled = request.getParameter("gracePeriod").equals("on");
 		
 		principal = Double.parseDouble(getServletContext().getInitParameter("principal"));
 		period = Double.parseDouble(getServletContext().getInitParameter("period"));
@@ -47,32 +42,23 @@ public class Start extends HttpServlet {
 		fixedInterest = Double.parseDouble(getServletContext().getInitParameter("fixedInterest"));
 		gracePeriod = Double.parseDouble(getServletContext().getInitParameter("gracePeriod"));
 		
-		varsInitialized = true;
+		//submit has been pressed, get the parameters that were entered
+		if (request.getParameter("submit") != null) {
+			principal = (request.getParameter("principal") == null) ? principal : Double.parseDouble(request.getParameter("principal"));
+			period = (request.getParameter("period") == null) ? period : Double.parseDouble(request.getParameter("period"));
+			interest = (request.getParameter("interest") == null) ? interest : Double.parseDouble(request.getParameter("interest"));
+		}
 		
-		principal = (request.getParameter("principal") == null) ? principal : Double.parseDouble(request.getParameter("principal"));
-		period = (request.getParameter("period") == null) ? period : Double.parseDouble(request.getParameter("period"));
-		interest = (request.getParameter("interest") == null) ? interest : Double.parseDouble(request.getParameter("interest"));
-
-		savedPrincipal = principal;
-		savedPeriod = period;
 		
 		PrintWriter p = response.getWriter();
 		
-		if (request.getRequestURI().contains("/Startup/YorkBank")) {
-			response.sendRedirect(this.getServletContext().getContextPath() + "/Start");
-		}
-		
-		//p.append("\nContext arg: ").append(this.getServletContext().getInitParameter("Param1"));
-		
-		p.append("\n---- Monthly Payments ----");
-		p.append("\nBased on Principal=" + principal + " Period=" + period + 
-				" Interest=" + interest);
-		
-		double monthlyInt = (interest / 12.0) * .01;
-		//double monthlyPayments = principal * Math.pow(1+monthlyInt, period) * (monthlyInt)
-		//		/ (Math.pow(1+monthlyInt, period) - 1);
-		
+		double monthlyInt = ((fixedInterest + interest) / 12.0) * .01;
 		double monthlyPayments = monthlyInt * principal / (1 - Math.pow(1+monthlyInt, -period));
+		
+		/*if (gracePeriodEnabled) {
+			graceInterest = principal * ((interest + fixedInterest) / 12.0) * gracePeriod;
+			monthlyPayments += (graceInterest / gracePeriod);
+		}*/
 		
 		DecimalFormat d = new DecimalFormat("##.0");
 		p.append("\nMonthly payments: " + d.format(monthlyPayments));
